395f87be0a5af8cf4e8facc44527f165b819c31a
Joe Hos <joehoskisson@gmail.com>
2017-09-19 16:04:52 -0600

    added timing ease at end of red fill-in. changed size of rectangles to 10 from 12

--- src/components/main/ideabox/index.js
+++ src/components/main/ideabox/index.js
@@ -30,16 +30,24 @@ class IdeaboxContainer extends React.Component {
     if(this.isMouseDown) {
       var X = e.pageX - e.target.offsetLeft;
       var Y = e.pageY - e.target.offsetTop;
-      this.ctx.fillRect(X, Y, 12, 12);
+      this.ctx.fillRect(X, Y, 10, 10);
       this.drawArr.push([X,Y]);
     }
   }
 
   pizzazzIt = () => {
     this.ctx.fillStyle = "#FF0000";
+    // drawing speed increases until slowMark, then it slows
+    let slowMark = 3000 / (1 + this.drawArr.length * 0.9 * 2);
     for(let i = 0; i < this.drawArr.length; i++) {
-      let secs = 3000 / (1 + i * 10);
-      setTimeout(()=>{this.ctx.fillRect(this.drawArr[i][0], this.drawArr[i][1], 12, 12), secs})
+      let secs = 3000 / (1 + i * 2);
+      if(i / this.drawArr.length > .9) {
+        // when i is 90% of this.drawArr.length, (i / (this.drawArr.length * .9))
+        // is roughly 1. It gradually increases as i gets bigger. In the end close
+        // 10% of the 6000 gets added to slowMark.
+        secs = slowMark + (i / (this.drawArr.length * .9)) * 6000 - 6000;
+      }
+      setTimeout(()=>{this.ctx.fillRect(this.drawArr[i][0], this.drawArr[i][1], 10, 10), secs})
       //this.ctx.fillRect(this.drawArr[i][0], this.drawArr[i][1], 12, 12);
     }
   }
