458657f4be3ceef0e44786136634ba29a8aad69d
Joe Hos <joehoskisson@gmail.com>
2017-09-19 16:50:40 -0600

    reorganized drawEffects and began fringe. Discovered bug in secs, replaced secs with just 1000 in timeout

--- src/components/main/ideabox/index.js
+++ src/components/main/ideabox/index.js
@@ -37,19 +37,55 @@ class IdeaboxContainer extends React.Component {
 
   pizzazzIt = () => {
     this.ctx.fillStyle = "#FF0000";
-    // drawing speed increases until slowMark, then it slows
-    let slowMark = 3000 / (1 + this.drawArr.length * 0.9 * 2);
-    for(let i = 0; i < this.drawArr.length; i++) {
-      let secs = 3000 / (1 + i * 2);
-      if(i / this.drawArr.length > .9) {
-        // when i is 90% of this.drawArr.length, (i / (this.drawArr.length * .9))
-        // is roughly 1. It gradually increases as i gets bigger. In the end close
-        // 10% of the 6000 gets added to slowMark.
-        secs = slowMark + (i / (this.drawArr.length * .9)) * 6000 - 6000;
-      }
-      setTimeout(()=>{this.ctx.fillRect(this.drawArr[i][0], this.drawArr[i][1], 10, 10), secs})
-      //this.ctx.fillRect(this.drawArr[i][0], this.drawArr[i][1], 12, 12);
+
+
+    let recolor = (i) => {
+      this.ctx.fillRect(this.drawArr[i][0], this.drawArr[i][1], 10, 10)
     }
+
+    let drawFringe = (i) => {
+      if(i > 0 && i % 5 === 0){
+        let x = this.drawArr[i - 2][0];
+        let y = this.drawArr[i - 2][1];
+        let slope = (this.drawArr[i][1] - this.drawArr[i - 5][1])/(this.drawArr[i][0] - this.drawArr[i - 5][0]);
+        if(slope > 0.5 && slope < 10) {
+          // positive
+
+          // left of line
+          this.ctx.beginPath();
+          this.ctx.moveTo(x - 10, y + 10);
+          this.ctx.lineTo(x - 20, y + 20);
+          this.ctx.stroke();
+
+          // right of line
+          this.ctx.beginPath();
+          this.ctx.moveTo(x + 10, y - 10);
+          this.ctx.lineTo(x + 20, y - 20);
+          this.ctx.stroke();
+        }
+      }
+    }
+
+    let drawEffects = () => {
+      // drawing speed increases until slowMark, then it slows
+      let slowMark = 3000 / (1 + this.drawArr.length * 0.9 * 2);
+      for(let i = 0; i < this.drawArr.length; i++) {
+        let secs = 3000 / (1 + i * 2);
+        if(i / this.drawArr.length > .9) {
+          // when i is 90% of this.drawArr.length, (i / (this.drawArr.length * .9))
+          // is roughly 1. It gradually increases as i gets bigger. In the end close
+          // 10% of the 6000 gets added to slowMark.
+          secs = slowMark + (i / (this.drawArr.length * .9)) * 3000 - 3000;
+        }
+
+        // refill drawing with new color
+        setTimeout(()=>{this.ctx.fillRect(this.drawArr[i][0], this.drawArr[i][1], 10, 10)}, 1000)
+      }
+    }
+
+    // delay start of drawRefill
+    setTimeout(drawEffects, 500);
+
   }
 
   render() {
