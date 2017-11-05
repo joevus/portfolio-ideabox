d43fdee94f8ac30b138b025d499d02c3019fddb8
Joe Hos <joehoskisson@gmail.com>
2017-11-04 23:59:47 -0600

    started designing navbar better.

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,12 +3,13 @@ import React from "react";
 class Navbar extends React.Component {
   render() {
     return(
-      <div className="navbar">
+      <nav className="navbar">
         <div className="brand">Joe Hoskisson</div>
-        <div className="navLink"><a href="#">about</a></div>
-        <div className="navLink"><a href="#">projects</a></div>
-        <div className="navLink"><a href="#">contact</a></div>
-      </div>
+        <div className="nav-right">
+          <a className="navLink" href="#">Home</a>
+          <a className="navLink" href="#">About</a>
+        </div>
+      </nav>
     )
   }
 }
