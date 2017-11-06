02205cf22117887ce0b2345219a90bb945230369
Joe Hos <joehoskisson@gmail.com>
2017-11-06 16:42:18 -0700

    Working on making navbar responsive.

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,10 +4,21 @@ class Navbar extends React.Component {
   render() {
     return(
       <nav className="navbar">
-        <div className="brand"><a href="#">Joe Hoskisson</a></div>
-        <div className="nav-right">
-          <a className="navLink" href="#">Home</a>
-          <a className="navLink" href="#">About</a>
+        <div className="navbar-header">
+          <a className="brand" href="#">Joe Hoskisson</a>
+          <button className="navbar-toggle">
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </button>
+        </div>
+
+        <div className="navbar-collapse">
+          <ul>
+            <li><a className="navLink" href="#">Home</a></li>
+            <li><a className="navLink" href="#">About</a></li>
+          </ul>
         </div>
       </nav>
     )
