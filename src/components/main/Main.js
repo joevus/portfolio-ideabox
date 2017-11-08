df8f2d547f449050fe13e44134fd74f673bda155
Joe Hos <joehoskisson@gmail.com>
2017-11-08 16:34:09 -0700

    added header section

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,11 +1,13 @@
 import React from "react";
 
+import Header from "./header";
 import Ideabox from "./ideabox/index.js";
 
 class Main extends React.Component {
   render() {
     return (
       <div>
+          <Header />
           <Ideabox />
         <h1>Hi I'm Joe</h1>
       </div>
