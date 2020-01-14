e524cf7246d921320597cfe1d209f9790ee4beee
joevus <joehoskisson@gmail.com>
2020-01-13 17:04:07 -0700

    started work-grid formatting. Not mobile-ready

--- src/components/main/work/index.js
+++ src/components/main/work/index.js
@@ -5,9 +5,26 @@ class Work extends React.Component {
     return (
       <section className="work container">
         <div className="row">
-          <h1>See Work √</h1>
+          <h1>See Work <i className="fa fa-chevron-down"></i></h1>
         </div>
-
+        <div className="row work_grid">
+            <div className="work_entry">
+                <p>hi</p>
+                <p>hi</p>
+            </div>
+            <div className="work_entry">
+                <p>hi</p>
+                <p>hi</p>
+            </div>
+            <div className="work_entry">
+                <p>hi</p>
+                <p>hi</p>
+            </div>
+            <div className="work_entry">
+                <p>hi</p>
+                <p>hi</p>
+            </div>
+        </div>
       </section>
 
     );
