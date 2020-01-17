fae180ef8aa999e07514d013fdb0eb575cae1d26
joevus <joehoskisson@gmail.com>
2020-01-17 12:34:20 -0700

    added first draft of coverletter.

--- src/components/main/experience/index.js
+++ src/components/main/experience/index.js
@@ -8,10 +8,15 @@ class Experience extends React.Component {
           <h1>Work Experience</h1>
         </div>
         <div className="row coverletter">
-          <p>blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah </p>
-          <p>blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah </p>
-          <p>blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah </p>
-          <p>blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah </p>
+          <p>
+            With my experience as an entrepreneur, software engineer, and traveler for the last five years, I bring technical expertise as well as people and cultural skills to the teams I’m on.
+          </p>
+          <p>
+            I have a bachelors degree in chemical engineering and started learning web application development when I quit my chemical engineering job to start a business. Though the business did not thrive, that decision has led me down a road to teach coding in the Middle East, work for two companies as a software developer including MX, and most recently returning to the Middle East to become conversational in Arabic.
+          </p>
+          <p>
+            I can hold my own in backend engineering with Ruby, NodeJS and a variety of other technologies. I can also hold my own in a variety of cultures (I speak Spanish too), which could help your business expand into other regions of the world if you are into that. I also am always trying to get better at whatever I’m working on.
+          </p>
         </div>
 
       </section>
